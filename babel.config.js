6604f7a7c91e9b43bbca1bb2f49094f484f7edb6
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-08-02 11:48:53 -0700

    changed privy exports

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "nativewind/babel",
-      "react-native-reanimated/plugin",
-      // [
-      //   "module-resolver",
-      //   {
-      //     root: ["./"],
-      //     alias: {
-      //       "@": "./",
-      //     },
-      //   },
-      // ],
-    ],
+    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
   };
 };
