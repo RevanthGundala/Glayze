ce23a0b4599eabe08112fb344db1372c21a0e90a
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-08-07 15:19:51 -0700

    testing html

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,1 +1,9 @@
-<meta name="apple-itunes-app" content="app-id=6587549824" />;
+export default function Web() {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="apple-itunes-app" content="app-id=6587549824" />
+      </head>
+    </html>
+  );
+}
