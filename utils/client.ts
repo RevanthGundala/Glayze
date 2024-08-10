222efc64bf66702c3748576e57e6ae07b8f7267b
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-08-10 09:48:00 -0700

    debug

--- utils/client.ts
+++ utils/client.ts
@@ -1,20 +1,31 @@
-import "fast-text-encoding";
-import { createClient } from "@dynamic-labs/client";
-import { ReactNativeExtension } from "@dynamic-labs/react-native-extension";
-import { ViemExtension } from "@dynamic-labs/viem-extension";
+import { Platform } from "react-native";
 
-const environmentId =
-  process.env.EXPO_PUBLIC_ENVIRONMENT_ID ||
-  "850b02af-863c-4826-baaf-6e77b56b0dbd";
+let client;
 
-if (!environmentId) {
-  throw new Error("EXPO_PUBLIC_ENVIRONMENT_ID is required");
+if (Platform.OS !== "web") {
+  const { createClient } = require("@dynamic-labs/client");
+  const {
+    ReactNativeExtension,
+  } = require("@dynamic-labs/react-native-extension");
+  const { ViemExtension } = require("@dynamic-labs/viem-extension");
+
+  const environmentId =
+    process.env.EXPO_PUBLIC_ENVIRONMENT_ID ||
+    "850b02af-863c-4826-baaf-6e77b56b0dbd";
+
+  if (!environmentId) {
+    throw new Error("EXPO_PUBLIC_ENVIRONMENT_ID is required");
+  }
+
+  client = createClient({
+    environmentId,
+    appLogoUrl: "",
+    appName: "",
+  })
+    .extend(ReactNativeExtension())
+    .extend(ViemExtension());
+} else {
+  client = null;
 }
 
-export const client = createClient({
-  environmentId,
-  appLogoUrl: "",
-  appName: "",
-})
-  .extend(ReactNativeExtension())
-  .extend(ViemExtension());
+export { client };
