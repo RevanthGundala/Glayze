12ae46d35b9045269d8b89e2d34b4532a39b022a
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-09-23 10:53:04 -0700

    dbg

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,6 @@
 module.exports = {
-	globDirectory: 'app/',
-	globPatterns: [
-		'**/*.{tsx,ts}'
-	],
-	swDest: 'app/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: "app/",
+  globPatterns: ["**/*.{tsx,ts}"],
+  swDest: "app/dist/sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
